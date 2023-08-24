e8a86ceadda86b9562c000126fca5af86438107e
Confidence Elatech <confidence.wanogho@elatech.com.ng>
2023-08-24 15:59:23 +0100

    changes

--- package/bvn_package/lib/verification_succesful.dart
+++ package/bvn_package/lib/verification_succesful.dart
@@ -34,40 +34,44 @@ class _VerificationSuccessfulState extends State<VerificationSuccessful> {
       body: SafeArea(
         child: SingleChildScrollView(
             padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(height: size.height * 0.04),
-                  const SizedBox(height: 24),
-                  const Text("🥳Verification Successsful",
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                  const SizedBox(height: 8),
-                  const Text(
-                    "Your BVN verifiction was successful.. Thank You",
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(height: size.height * 0.04),
-                  Stack(
-                    clipBehavior: Clip.none,
-                    children: [
-                      Image.asset(
-                        loadAsset("success.png"),
-                        color: BVNPlugin.getBaseColor(),
-                        height: 300,
-                      ),
-                      Positioned(
-                        right: -34,
-                        bottom: 0,
-                        child: Image.asset(
-                          loadAsset("check.png"),
+            child: SizedBox(
+              width: double.infinity,
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(height: size.height * 0.04),
+                    const SizedBox(height: 24),
+                    const Text("🥳Verification Successsful",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold)),
+                    const SizedBox(height: 8),
+                    const Text(
+                      "Your BVN verifiction was successful.. Thank You",
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: size.height * 0.04),
+                    Stack(
+                      clipBehavior: Clip.none,
+                      children: [
+                        Image.asset(
+                          loadAsset("success.png"),
                           color: BVNPlugin.getBaseColor(),
-                          height: 300,
+                          height: 200,
                         ),
-                      ),
-                    ],
-                  )
-                ])),
+                        Positioned(
+                          right: 0,
+                          bottom: -16,
+                          child: Image.asset(
+                            loadAsset("check.png"),
+                            height: 54,
+                            width: 54,
+                            color: BVNPlugin.getBaseColor(),
+                          ),
+                        ),
+                      ],
+                    )
+                  ]),
+            )),
       ),
     );
   }
